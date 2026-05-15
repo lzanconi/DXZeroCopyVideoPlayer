e0070a20a33445eb066d981a4dc29202be52cdf8
lzanconi <zanconi@leva.io>
2026-05-15 09:35:22 +0200

    Removed unused code from main.cpp

--- DXZeroCopyVideoPlayer/main.cpp
+++ DXZeroCopyVideoPlayer/main.cpp
@@ -17,21 +17,6 @@ extern "C" {
 #include "DXRenderer.h"
 }
 
-// MANUALLY DEFINE THE INTERFACE IF HEADERS FAIL
-// This is the underlying COM definition for ID3D11Multithread
-#ifndef __ID3D11Multithread_INTERFACE_DEFINED__
-#define __ID3D11Multithread_INTERFACE_DEFINED__
-MIDL_INTERFACE("9b7e4e00-342c-4106-a19f-4f2704f689f0")
-ID3D11Multithread : public IUnknown
-{
-public:
-    virtual void STDMETHODCALLTYPE Enter(void) = 0;
-    virtual void STDMETHODCALLTYPE Leave(void) = 0;
-    virtual BOOL STDMETHODCALLTYPE SetMultithreadProtected(BOOL bMTProtect) = 0;
-    virtual BOOL STDMETHODCALLTYPE GetMultithreadProtected(void) = 0;
-};
-#endif
-
 // Linker pragmas (Alternative to Project Properties)
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "dxgi.lib")
@@ -41,80 +26,14 @@ public:
 #pragma comment(lib, "d3dcompiler.lib")
 
 
-// --- Globals ---
-IDXGISwapChain* g_SwapChain = nullptr;
-ID3D11Device* g_Device = nullptr;
-ID3D11DeviceContext* g_Context = nullptr;
-ID3D11RenderTargetView* g_RTV = nullptr;
-ID3D11PixelShader* g_PS = nullptr;
-ID3D11VertexShader* g_VS = nullptr;
-ID3D11SamplerState* g_Sampler = nullptr;
-ID3D11InputLayout* g_Layout = nullptr;
-ID3D11Buffer* g_VBuffer = nullptr;
-
 DXShader dxShader;
 DXRenderer* dxRenderer = nullptr;
-
-struct Vertex { float x, y, z; float u, v; };
 
 static enum AVPixelFormat get_hw_format(AVCodecContext* ctx, const enum AVPixelFormat* pix_fmts) {
     for (const enum AVPixelFormat* p = pix_fmts; *p != -1; p++) {
         if (*p == AV_PIX_FMT_D3D11) return *p;
     }
     return AV_PIX_FMT_NONE;
-}
-
-void InitD3D(HWND hwnd) {
-    DXGI_SWAP_CHAIN_DESC sd = {};
-    sd.BufferCount = 2; // Double buffering
-    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    sd.OutputWindow = hwnd;
-    sd.SampleDesc.Count = 1;
-    sd.Windowed = TRUE;
-    sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-
-    D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr,
-        D3D11_CREATE_DEVICE_VIDEO_SUPPORT, nullptr, 0, D3D11_SDK_VERSION, &sd, &g_SwapChain, &g_Device, nullptr, &g_Context);
-
-    // 1. ENABLE MULTITHREADING (Critical for FFmpeg)
-    ID3D11Multithread* mt = nullptr;
-    g_Device->QueryInterface(__uuidof(ID3D11Multithread), (void**)&mt);
-    if (mt) {
-        mt->SetMultithreadProtected(TRUE);
-        mt->Release();
-    }
-
-    ID3D11Texture2D* pBackBuffer;
-    g_SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
-    g_Device->CreateRenderTargetView(pBackBuffer, nullptr, &g_RTV);
-    pBackBuffer->Release();
-
-    if (!dxShader.Load(g_Device, L"shaders.hlsl")) {
-        fprintf(stderr, "ERROR: Failed to load shaders.\n");
-        return;
-    }
-
-    Vertex vertices[] = {
-        { -1,  1, 0, 0, 0 }, {  1,  1, 0, 1, 0 }, { -1, -1, 0, 0, 1 },
-        {  1,  1, 0, 1, 0 }, {  1, -1, 0, 1, 1 }, { -1, -1, 0, 0, 1 }
-    };
-    D3D11_BUFFER_DESC bd = { sizeof(vertices), D3D11_USAGE_DEFAULT, D3D11_BIND_VERTEX_BUFFER };
-    D3D11_SUBRESOURCE_DATA initData = { vertices };
-    g_Device->CreateBuffer(&bd, &initData, &g_VBuffer);
-
-    D3D11_INPUT_ELEMENT_DESC ied[] = {
-        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 }
-    };
-    g_Device->CreateInputLayout(ied, 2, dxShader.VSBytecode->GetBufferPointer(), 
-        dxShader.VSBytecode->GetBufferSize(), &g_Layout);
-
-    D3D11_SAMPLER_DESC sampDesc = {};
-    sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
-    sampDesc.AddressU = sampDesc.AddressV = sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
-    sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
-    g_Device->CreateSamplerState(&sampDesc, &g_Sampler);
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
@@ -139,7 +58,6 @@ int main() {
     wc.hInstance = GetModuleHandle(NULL);
     RegisterClass(&wc);
     HWND hwnd = CreateWindow(L"VP", L"Zero-Copy Player (Refactored)", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 1280, 720, 0, 0, wc.hInstance, 0);
-    //InitD3D(hwnd);
 
     char exePath[MAX_PATH] = {};
     GetModuleFileNameA(nullptr, exePath, MAX_PATH);
